27108a42c9e56ad8734a09bd0e5fde945f398260
TheUberKid <zekixu@gmail.com>
2019-05-11 01:35:43 -0700

    added links to future pages

--- public/js/home.js
+++ public/js/home.js
@@ -1,10 +1,22 @@
 // so we can feel like we're using jquery
 function $(query){
-  return document.querySelector(query);
+  var q = document.querySelectorAll(query);
+  return q.length > 1 ? q : q[0];
 }
 
+// make the reflection match the sun!
 function scrollUpdate(){
   $('#sun-reflection').style.marginTop = $('#water').getBoundingClientRect().top - $('#sun').getBoundingClientRect().bottom + 'px';
 }
 window.addEventListener('scroll', scrollUpdate);
+window.addEventListener('resize', scrollUpdate);
 scrollUpdate();
+
+// make album links work
+var albums = $('#album-grid .item');
+for(var i in albums){
+  if(!albums[i].dataset) break;
+  albums[i].addEventListener('click', function(e){
+    window.location.href = 'music/' + e.currentTarget.dataset.target;
+  });
+}
