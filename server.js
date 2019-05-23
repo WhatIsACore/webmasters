11ba7df92aa8e1665df2798a54b08b76ba18d207
Zeki Xu <zekixu@gmail.com>
2019-05-23 08:40:03 -0700

    Update server.js

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ routes.use(compression())
       .get('/', function(req, res, next){
         res.sendFile(__dirname + '/public/index.html');
       })
-      .get('/a.jpg', function(req, res, next){
+      .get('/a.png', function(req, res, next){
         logger.info('ping image requested: ' + decodeURIComponent(req.query.a));
         res.sendFile(__dirname + '/public/media/map.png');
       })
